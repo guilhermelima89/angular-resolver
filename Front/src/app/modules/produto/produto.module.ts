a62c5243377ecd0938127d9ec1190b4f32379c90
Guilherme <guyga89@gmail.com>
2022-08-20 21:11:20 -0300

    removendo component count

--- Front/src/app/modules/produto/produto.module.ts
+++ Front/src/app/modules/produto/produto.module.ts
@@ -3,15 +3,14 @@ import { NgModule } from '@angular/core';
 import { FuseAlertModule } from '@fuse/components/alert';
 import { MaterialModule } from 'app/shared/material.module';
 import { SharedModule } from 'app/shared/shared.module';
-import { CountComponent } from './components/count/count.component';
+import { DetailsComponent } from './components/details/details.component';
 import { FormComponent } from './components/form/form.component';
 import { TableComponent } from './components/table/table.component';
+import { PageDetailsComponent } from './pages/page-details/page-details.component';
 import { PageListComponent } from './pages/page-list/page-list.component';
 import { ProdutoRoutingModule } from './produto-routing.module';
-import { DetailsComponent } from './components/details/details.component';
-import { PageDetailsComponent } from './pages/page-details/page-details.component';
 @NgModule({
-  declarations: [PageListComponent, FormComponent, TableComponent, CountComponent, DetailsComponent, PageDetailsComponent],
+  declarations: [PageListComponent, FormComponent, TableComponent, DetailsComponent, PageDetailsComponent],
   imports: [CommonModule, ProdutoRoutingModule, MaterialModule, SharedModule, FuseAlertModule],
 })
 export class ProdutoModule {}
